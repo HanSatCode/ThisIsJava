4c5b1436794952e68fae5c78344049bcf80ddc96
Han028 <han028@yu.ac.kr>
2025-08-17 17:10:15 +0900

    add ch07 & ch08

---
+++ src/ch08/verify/Cat.java
@@ -0,0 +1,8 @@
+package ch08.verify;
+
+public class Cat implements Soundable {
+	@Override
+	public String sound() {
+		return "야옹";
+	}
+}
---
+++ src/ch08/verify/Dog.java
@@ -0,0 +1,8 @@
+package ch08.verify;
+
+public class Dog implements Soundable {
+	@Override
+	public String sound() {
+		return "멍멍";
+	}
+}
